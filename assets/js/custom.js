454b2f866d228434741a45162e8286e961da7d32
MadiyarZh <mzhorakhan00@gmail.com>
2022-06-06 13:08:27 +0600

    adaptive pages (css)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,29 +1,29 @@
 
-document.addEventListener("scroll", handleScroll);
-// get a reference to our predefined button
-var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+// document.addEventListener("scroll", handleScroll);
+// // get a reference to our predefined button
+// var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 
-function handleScroll() {
-  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var GOLDEN_RATIO = 0.5;
+// function handleScroll() {
+//   var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   var GOLDEN_RATIO = 0.5;
 
-  if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
-    //show button
-    scrollToTopBtn.style.display = "block";
-  } else {
-    //hide button
-    scrollToTopBtn.style.display = "none";
-  }
-}
+//   if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
+//     //show button
+//     scrollToTopBtn.style.display = "block";
+//   } else {
+//     //hide button
+//     scrollToTopBtn.style.display = "none";
+//   }
+// }
 
-scrollToTopBtn.addEventListener("click", scrollToTop);
+// scrollToTopBtn.addEventListener("click", scrollToTop);
 
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-}
+// function scrollToTop() {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth"
+//   });
+// }
 
 
 $(document).ready(function() {
